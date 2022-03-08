cc31ee05e94218b88011256dbc763714672f4b7c
samah37 <hs_kansab@esi.dz>
2022-03-08 13:20:25 +0100

    added: web file

--- Item.py
+++ Item.py
@@ -1,8 +1,1 @@
-from pydantic import BaseModel
-from typing import Optional
 
-class Item(BaseModel):
-    name: str
-    description: Optional[str] = None
-    price: float
-    tax: Optional[float] =  None
--- main.py
+++ main.py
@@ -1,17 +1,26 @@
-from fastapi import FastAPI, Query, Path, Body, Cookie
 from enum import Enum
-from pydantic import BaseModel, Field
 from typing import Optional
-from Item import Item
 
+from fastapi import FastAPI, Query, Path, Body
+from pydantic import BaseModel
+from pydantic import Field
+
+
+class Item(BaseModel):
+    name: str
+    description: Optional[str] = None
+    price: float
+    tax: Optional[float] = None
 
 
 class ModelName(str, Enum):
     alexnet = "alexnet"
     resnet = "resnet"
-    lenet =  "lenet"
+    lenet = "lenet"
+
 
 app = FastAPI()
+
 
 @app.get("/")
 async def root():
@@ -22,9 +31,11 @@ async def root():
 async def say_hello(name: str):
     return {"message": f"Hello {name}"}
 
+
 @app.get("/items/{item_id}")
 async def read_item(item_id):
     return {"item_id": item_id}
+
 
 @app.get("/models/{model_name}")
 async def get_model(model_name: ModelName):
@@ -32,89 +43,95 @@ async def get_model(model_name: ModelName):
         return {"model_name": model_name, "message": "Deep Learning FTW!"}
 
     if model_name.value == "lenet":
-            return {"model_name": model_name, "message": "LeCNN all the images"}
+        return {"model_name": model_name, "message": "LeCNN all the images"}
 
     return {"model_name": model_name, "message": "Have some residuals"}
+
 
 @app.get("/files/{file_path: path}")
 async def read_file(file_path: str):
     return {"file_path": file_path}
+
+
 fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+
+
 @app.get("/items/")
 async def read_item(skip: int = 0, limit: int = 10):
-    return fake_items_db[skip : skip + limit]
+    return fake_items_db[skip: skip + limit]
+
+
 from typing import Optional
+
+
 @app.get("/items/{item_id}")
 async def read_item(item_id: str, q: Optional[str] = None, short: bool = False):
-    item = {"item_id" : item_id}
+    item = {"item_id": item_id}
     if q:
-        item.update({"q":q})
-    if not short:
-        item.update(
-            {"description": "This is an amazing item that has a long description"}
-        )
-    return item
-@app.get("/users/{user_id}/items/{item_id}")
-async def read_user_item(
-    user_id: int, item_id: str, q: Optional[str] = None, short: bool = False
-):
-    item = {"item_id": item_id, "owner_id": user_id}
-    if q:
-        item.update({"q":q})
+        item.update({"q": q})
     if not short:
         item.update(
             {"description": "This is an amazing item that has a long description"}
         )
     return item
 
+
+@app.get("/users/{user_id}/items/{item_id}")
+async def read_user_item(
+        user_id: int, item_id: str, q: Optional[str] = None, short: bool = False
+):
+    item = {"item_id": item_id, "owner_id": user_id}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update(
+            {"description": "This is an amazing item that has a long description"}
+        )
+    return item
+
+
 @app.post("/item_test/{item_id}")
-async def create_item(item_id: int, item: Item, q: Optional[str]= None):
-    item.name =  "hihi"
+async def create_item(item_id: int, item: Item, q: Optional[str] = None):
+    item.name = "hihi"
     result = {"item_id": item_id, **item.dict()}
     if q:
         result.update({"q": q})
     return result
 
+
 @app.get("/items_test/")
-async def read_items(q: Optional[str]= Query(None, max_length=50)):
-    results =  {"items": [{"item_id": "Foo"}, {"item"}]}
+async def read_items(q: Optional[str] = Query(None, max_length=50)):
+    results = {"items": [{"item_id": "Foo"}, {"item"}]}
     if q:
         results.update({"q": q})
     return results
 
+
 @app.get('/items_path/{item_id}')
 async def read_items(
-        item_id : int = Path(..., title="The ID of the item to get"),
-        q: Optional[str] =  Query(None, alias="item-query")
+        item_id: int = Path(..., title="The ID of the item to get"),
+        q: Optional[str] = Query(None, alias="item-query")
 ):
     results = {"item_id": item_id}
     if q:
-        results.update({"q" : q})
+        results.update({"q": q})
     return results
+
 
 class User(BaseModel):
     username: str
     full_name: Optional[str] = None
 
+
 @app.put("/items/{item_id}")
-async  def update_item(
-        item_id: int, item: Item, user: User, importance: int= Body(...)
+async def update_item(
+        item_id: int, item: Item, user: User, importance: int = Body(...)
 ):
-    results = {"item_id": item_id, "item": item, "user": user, "importance": importance }
+    results = {"item_id": item_id, "item": item, "user": user, "importance": importance}
     return results
+
+
 class ItemField(BaseModel):
     name: str
-    description: Optional[str] =  Field(None, title = "the description of the item", max_length=300)
+    description: Optional[str] = Field(None, title="the description of the item", max_length=300)
     price: float = Field(..., gt=0, description="the price must hh")
-
-
-
-
-
-
-
-
-
-
-
-
